aff7ee273bbe189155d35a2b13aa687b17ef1b14
Pastor Guillaume <guillaume.pastor@fms-ea.com>
2023-02-08 16:04:42 +0100

    exo1.2

--- src/fr/fms/poo/City.java
+++ src/fr/fms/poo/City.java
@@ -13,30 +13,65 @@ package fr.fms.poo;
 public class City {
 
 	// Add attributs
-	String town;
-	String country;
-	Integer nbInhabitants;
+	 public String town;
+	 public String country;
+	 private Integer nbInhabitants; // en mettant en private on passe par les Setters
 
 	
 
 	// Create Constructeur
-	public City(String town, String country, Integer nbInhabitants) {
+//	public City(String town, String country, Integer nbInhabitants) {
+//		this.town = town;
+//		this.country  = country;
+//		this.nbInhabitants = nbInhabitants; 
+//  }
+	
+	// Create Constructeur exo1.2
+	 public City(String town, String country, Integer nbInhabitants) {
+		 setTown(town);
+		 setCountry(country);
+		 setNbInhabitants(nbInhabitants);
+	 }
+	// Create Getters and Setters exo1.2
+	public String getTown() {
+		return town;
+	}
+
+
+	public void setTown(String town) {
 		this.town = town;
-		this.country  = country;
-		this.nbInhabitants = nbInhabitants; 
-
-	}
-	
-
-	
-	
-	// Create Méthode
-	public void display()
-	{
-		System.out.print(" Town : "+ this.town  + " \t" +
-				 " Country : "+ this.country + " \t" +  " NbInhabitants : " + this.nbInhabitants +" \t\n");
 	}
 
+
+	public String getCountry() {
+		return country;
+	}
+
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+
+	public Integer getNbInhabitants() {
+		return nbInhabitants;
+	}
+
+
+	public void setNbInhabitants(Integer nbInhabitants) {
+		if(nbInhabitants < 0)
+			throw new RuntimeException("La poulation ne peut pas être négative !!");
+		this.nbInhabitants = nbInhabitants;
+	}
+
+
+	// Create Méthode exo1.2
+	
+
+		public String toString() {
+			return "Town : " + getTown() + "\t" + "Country : " + getCountry() + "\t" + "NbInhabitants : "
+		+ this.getNbInhabitants();
+			}
 	
 }
 
--- src/fr/fms/poo/TestsCity.java
+++ src/fr/fms/poo/TestsCity.java
@@ -12,29 +12,37 @@ package fr.fms.poo;
 
 //Create classe Tests
 public class TestsCity {
-	
-	
-	
-//  Main starts
-	public static void main(String[] args) {
-		
-		
-		 City paris = new City("Paris","France" ,2000000);
-		 City nice = new City("Nice", "France" ,340000); 
-		 City toulouse = new City("Toulouse", "France",450000);
-		 City grenoble = new City("Grenoble", "France",454759 );
-		 
 
-		 paris.display();
-		 nice.display();
-		 System.out.println("***********************************************************************");
-		 toulouse.display();
-		 grenoble.display();
-		 System.out.println("***********************************************************************");
-		 System.out.println("Après ajout");
-		 toulouse.nbInhabitants += 20000;
-		 toulouse.display();
-		 	 	 
+
+
+	//  Main starts
+	public static void main(String[] args) {
+
+		City paris = new City("Paris","France" ,2000000);
+		City nice = new City("Nice", "France" ,340000); 
+		City toulouse = new City("Toulouse", "France",450000);
+		City grenoble = new City("Grenoble", "France",454759 );
+		City bordeaux = new City ("Bordeaux", "France",249712 );
+
+		//paris.display();
+		//nice.display();
+		System.out.println("***********************************************************************");
+		//toulouse.display();
+		//grenoble.display();
+		System.out.println("***********************************************************************");
+		//System.out.println("Après ajout");
+		//toulouse.nbInhabitants += 20000;
+		//toulouse.display();
+		
+		// exo1.2
+		System.out.println(bordeaux + "\n");
+		//System.out.println(bordeaux);
+		
+		/*
+		 * On ne peut pas rajouter ou changer la population car 'nbInhabitants' est en private.
+		 * toulouse.nbInhabitants -= 20000; 
+		 * **/
+		bordeaux.setNbInhabitants(bordeaux.getNbInhabitants());
 	}
 
 }
